38eb89737324a64b943f838e913164e188ff66a4
Jeff Wack <jeffwack111@gmail.com>
2022-06-07 14:34:17 -0400

    now generates animation for kneading sequences of arbitrary degree. Kneading sequences are returned as a list of numbers, and the list of colors is generated randomly.

--- kneading.py
+++ kneading.py
@@ -2,43 +2,50 @@ from matplotlib.patches import Wedge
 import matplotlib.pyplot as plt
 from matplotlib.animation import ArtistAnimation
 import numpy as np
+from random import randint
 
-def kneading_seq(theta):
+def kneading_seq(theta,degree):
+
+    boundaries = [0]
+
+    for bb in range(degree+1):
+        boundaries.append((theta[0]+bb*theta[1])/degree)
+
+    boundaries.append(theta[1])
 
     seq = []
     K = []
 
-    a = theta[0]/2             #the two boundaries of the domains 'A' and 'B'
-    b = (theta[0]+theta[1])/2
-
     t = theta
     n = 0
-    while seq.count(t) < 1: #When we arrive at an angle already in the list we already know what comes next. The 
+
+    while seq.count(t) < 1: #When we arrive at an angle already in the list we already know what comes next. 
         n = n+1
         seq.append(t)
 
-        if t[0] < a:
-            K.append('B')
-        elif t[0] == a:
-            K.append('2')
-        elif t[0] > a and t[0] < b:
-            K.append('A')
-        elif t[0] == b:
-            K.append('1')
-        elif t[0] > b:
-            K.append('B')
+        for qq in range(degree+1):
+            if t[0] == boundaries[qq]:
+                K.append((2*qq)%(2*degree))
+            elif boundaries[qq] < t[0] and t[0] < boundaries[qq+1]:
+                K.append((2*qq-1)%(2*degree))
 
-        t = [t[0]*2%t[1],t[1]]  #multiply the angle by 2 modulo 1
+        t = [t[0]*degree%t[1],t[1]]  #multiply the angle by degree modulo 1
 
     return K
 
-colora = 'blue'
-colorb = 'red'
-color1 = 'green'
-color2 = 'pink'
+
+
+colorlist = []
+n = 100
+
+for i in range(n):
+    colorlist.append('#%06X' % randint(0, 0xFFFFFF))
+
+
+degree = 4
 
 start = 1
-end = 311
+end = 101
 inter = 100
 r = 20
 
@@ -46,24 +53,14 @@ AllFrames = []
 fig, ax = plt.subplots(figsize = (10,10))
 for denominator in range(start,end):
     frame = []
-
     for numerator in range(denominator):
         
-        seq = kneading_seq([numerator,denominator])
+        seq = kneading_seq([numerator,denominator],degree)
         #print(seq)
         n_layers = len(seq)
         for layer, char in enumerate(seq):
             if layer<np.sqrt(2)*r/2:
-                if char == 'A':
-                    wedgecolor = colora
-                elif char == 'B':
-                    wedgecolor = colorb
-                elif char == '1':
-                    wedgecolor = color1
-                elif char == '2':
-                    wedgecolor = color2
-                else:
-                    wedgecolor = 'black'
+                wedgecolor = colorlist[char]
                 wedge = Wedge((0, 0), layer+1, 360*(numerator - 0.5)/denominator,360*(numerator + 0.5)/denominator, 1, color = wedgecolor )    
                 ax.add_artist(wedge)
                 frame.append(wedge)
@@ -78,4 +75,4 @@ ax.set_ylim([-r,r])
 plt.axis('off')
 loop = AllFrames + AllFrames[::-1]
 anim = ArtistAnimation(fig,loop,interval = inter)
-anim.save('kneading{}to{}_{}.gif'.format(start,end,inter))
+anim.save('kneading_degree{}_{}to{}_{}.gif'.format(degree,start,end,inter)) 
